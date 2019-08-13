896819da61c63ddc3decf780f9519b32bc2c3fac
Sailal Goparaju <sailal.goparaju@hcl.com>
2019-08-13 01:08:31 -0400

    section 3

---
+++ ppmtool/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Projectitem from "./Project/Projectitem";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1> hello </h1>
+        <Projectitem />
+      </div>
+    );
+  }
+}
+export default Dashboard;
---
+++ ppmtool/src/components/Project/Projectitem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Projectitem extends Component {
+  render() {
+    return <h1> Project item </h1>;
+  }
+}
+
+export default Projectitem;
